e9b0be57431d776bd8ddd497a7c9ebd7add14bbd
Adam Rosas <adamrosas27@gmail.com>
2023-10-01 16:21:33 -0700

    Add logic to tsp method that will take in all the route distances and return the shortest distance possible

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ class Main:
     # Create function that will determine each possible route and holds some key logic for determining the shortest route
 
     # Should take in the adj_matrix and the route as parameters
-    def distance(route, adj_matrix):
+    def distances(route, adj_matrix):
         # Initialize a variable that will hold the total distance of the route
         total_distance = 0
 
@@ -22,12 +22,6 @@ class Main:
 # Create a helper function that will receive the possible routes from the routes method and execute the traveling salesman algorithm
     def tsp(self):
 
-        # Initialize some variables that will be used to hold the best route and all possible routes using permutations from the itertools library
-
-        shortest_route = None
-        cities = len(adj_matrix)
-        all_routes = permutations(range(cities))
-
         # Add my 2D matrix that will represent my graph and the elements within will represent the distance between each city
 
         adj_matrix = [
@@ -38,6 +32,23 @@ class Main:
             [2, 15, 6, 2, 0, 4],
             [10, 1, 0, 0, 4, 0]
         ]
+
+        # Initialize some variables that will be used to hold the best route and all possible routes using permutations from the itertools library
+
+        shortest_route = None
+        cities = len(adj_matrix)
+        all_routes = permutations(range(cities))
+
+        # Add a for loop that will go through all the possible routes in all_routes and call the distance function to determine the shortest route
+        for route in all_routes:
+            # If the shortest route is still uninitialized then it will be set to the first route in all_routes
+            if shortest_route == None:
+                shortest_route = route
+            else:
+                # Otherwise, call the distance function to determine the shortest route and then update the shortest_route variable to represent the new value
+                if distances(route, adj_matrix) < distances(shortest_route, adj_matrix):
+                    shortest_route = route
+        return shortest_route
 
 # Create a main function that will run the program
     def main(self):
