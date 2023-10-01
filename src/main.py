01e8a7b5e9abb4973ad352c86f80f2e1dcda32b0
Adam Rosas <adamrosas27@gmail.com>
2023-10-01 14:25:28 -0700

    Add some python boilerplate methods to main class

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
 # Create a main class that will be used to hold my routes function, my function that executes the traveling salesman algorithm, and my main function that will run the program
+class Main:
 
-# Create function that will determine each possible route and holds some key logic for determining the shortest route
+    # Create function that will determine each possible route and holds some key logic for determining the shortest route
+    def routes(self):
+        pass
 
 # Create a helper function that will receive the possible routes from the routes method and execute the traveling salesman algorithm
+    def tsp(self):
+        pass
 
 # Create a main function that will run the program
+    def main(self):
+        pass
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.main()
