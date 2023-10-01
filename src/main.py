4685c213a2fad9510ec559cf7febf955bd8a77e4
Adam Rosas <adamrosas27@gmail.com>
2023-10-01 14:21:27 -0700

    Add comments planning out class methods in src/main.py

---
+++ src/main.py
@@ -0,0 +1,7 @@
+# Create a main class that will be used to hold my routes function, my function that executes the traveling salesman algorithm, and my main function that will run the program
+
+# Create function that will determine each possible route and holds some key logic for determining the shortest route
+
+# Create a helper function that will receive the possible routes from the routes method and execute the traveling salesman algorithm
+
+# Create a main function that will run the program
