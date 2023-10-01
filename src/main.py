72ddfe05eca29db605c275def1b2af94276d2000
Adam Rosas <adamrosas27@gmail.com>
2023-10-01 15:38:39 -0700

    import permutations from itertools to be able to generate all possible routes

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
+from itertools import permutations
+
 # Create a main class that will be used to hold my routes function, my function that executes the traveling salesman algorithm, and my main function that will run the program
+
+
 class Main:
 
     # Create function that will determine each possible route and holds some key logic for determining the shortest route
@@ -8,14 +12,17 @@ class Main:
 # Create a helper function that will receive the possible routes from the routes method and execute the traveling salesman algorithm
     def tsp(self):
 
+        # Initialize some variables that will be used to hold the best route and all possible routes using permutations from the itertools library
+
         # Add my 2D matrix that will represent my graph and the elements within will represent the distance between each city
+
         adj_matrix = [
             [0, 0, 3, 0, 2, 10],
             [0, 0, 0, 0, 15, 1],
             [3, 0, 0, 7, 6, 0],
             [0, 0, 7, 0, 2, 0],
             [2, 15, 6, 2, 0, 4],
-            [10, 1, 0, 0, 4, 0],
+            [10, 1, 0, 0, 4, 0]
         ]
 
 # Create a main function that will run the program
