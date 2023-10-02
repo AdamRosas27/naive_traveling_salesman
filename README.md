af1805a0290a97f9963ffcbbff26724d68ab5833
Adam Rosas <adamrosas27@gmail.com>
2023-10-01 21:09:16 -0700

    Update README and add route method functionality

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# naive_traveling_salesman
-A naive approach to the traveling salesman problem utilizing connectivity matrices. 
+# Traveling Salesman Problem Solver
+
+This Python program focuses on solving the Traveling Salesman Problem (TSP) using a brute-force approach. It finds the shortest possible route that visits a given set of cities and returns to the starting city. The project also provides a basic structure for organizing and managing the code.
+
+## Table of Contents
+
+- [Traveling Salesman Problem Solver](#traveling-salesman-problem-solver)
+  - [Table of Contents](#table-of-contents)
+  - [Introduction](#introduction)
+  - [Features](#features)
+  - [Running the TSP Algorithm](#running-the-tsp-algorithm)
+  - [License](#license)
+  - [Acknowledgments](#acknowledgments)
+
+## Introduction
+
+The code in this repository solves the Traveling Salesman Problem (TSP) using a brute-force approach. It calculates the shortest route that visits a specified set of cities and returns to the starting city. The program defines a `Main` class that contains functions for determining possible routes and finding the shortest route using permutations from the `itertools` library. 
+
+## Features
+
+- Calculation of the shortest route for the Traveling Salesman Problem.
+- Efficient use of permutations to explore all possible routes.
+- Demonstrates a basic structure for organizing Python code.
+
+## Running the TSP Algorithm 
+```python
+if __name__ == "__main__":
+    main = Main()
+    main.main()
+```
+## License
+N/A
+
+## Acknowledgments
+- Adam Rosas
--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+""" Adam Rosas CSC 401: Assignment - Traveling Salesman Problem """
+
 from itertools import permutations
 
 # Create a main class that will be used to hold my routes function, my function that executes the traveling salesman algorithm, and my main function that will run the program
@@ -8,7 +10,7 @@ class Main:
     # Create function that will determine each possible route and holds some key logic for determining the shortest route
 
     # Should take in the adj_matrix and the route as parameters
-    def distances(route, adj_matrix):
+    def distances(self, route, adj_matrix):
         # Initialize a variable that will hold the total distance of the route
         total_distance = 0
 
@@ -20,18 +22,7 @@ class Main:
         return total_distance
 
 # Create a helper function that will receive the possible routes from the routes method and execute the traveling salesman algorithm
-    def tsp(self):
-
-        # Add my 2D matrix that will represent my graph and the elements within will represent the distance between each city
-
-        adj_matrix = [
-            [0, 0, 3, 0, 2, 10],
-            [0, 0, 0, 0, 15, 1],
-            [3, 0, 0, 7, 6, 0],
-            [0, 0, 7, 0, 2, 0],
-            [2, 15, 6, 2, 0, 4],
-            [10, 1, 0, 0, 4, 0]
-        ]
+    def tsp(self, distances, adj_matrix):
 
         # Initialize some variables that will be used to hold the best route and all possible routes using permutations from the itertools library
 
@@ -52,7 +43,24 @@ class Main:
 
 # Create a main function that will run the program
     def main(self):
-        pass
+
+        # Add my 2D matrix that will represent my graph and the elements within will represent the distance between each city
+        adj_matrix = [
+            [0, 0, 3, 0, 2, 10],
+            [0, 0, 0, 0, 15, 1],
+            [3, 0, 0, 7, 6, 0],
+            [0, 0, 7, 0, 2, 0],
+            [2, 15, 6, 2, 0, 4],
+            [10, 1, 0, 0, 4, 0]
+        ]
+
+        # Call the tsp function to execute the traveling salesman algorithm
+        shortest_possible_route = self.tsp(self.distances, adj_matrix)
+        route_distance = self.distances(shortest_possible_route, adj_matrix)
+        # Print the shortest route
+        print(f"Shortest Route: {shortest_possible_route}")
+        # Print the distance of the shortest route
+        print(f"Distance: {route_distance}")
 
 
 if __name__ == "__main__":
